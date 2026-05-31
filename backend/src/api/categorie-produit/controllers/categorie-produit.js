7facf5225fd10197ecbe7af0bcd1aff1c9af312b
AssoTakin <takainsideasso@gmail.com>
2026-05-31 05:39:39 +0200

    fix: categorie-produit controller - correct factories import

--- backend/src/api/categorie-produit/controllers/categorie-produit.js
+++ backend/src/api/categorie-produit/controllers/categorie-produit.js
@@ -1,4 +1,4 @@
 'use strict';
-const { createCoreController } = require('@strapi/strapi/factories');
+const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::categorie-produit.categorie-produit');
