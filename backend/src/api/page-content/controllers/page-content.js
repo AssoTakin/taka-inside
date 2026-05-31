7f5bbf9304de0097a47c1d025218954900a3fc44
AssoTakin <takainsideasso@gmail.com>
2026-05-31 04:56:02 +0200

    fix: page-content controller - use @strapi/strapi/factories

--- backend/src/api/page-content/controllers/page-content.js
+++ backend/src/api/page-content/controllers/page-content.js
@@ -1,4 +1,4 @@
 'use strict';
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require('@strapi/strapi/factories');
 
 module.exports = createCoreController('api::page-content.page-content');
